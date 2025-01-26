71f44fd7bbf469d286b2f1696757283db0c5b6ef
chandra babu <alekhya.gsb@gmail.com>
2025-01-27 02:21:10 +0530

    Assignment1 - Data Type

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -1,9 +1,55 @@
 package com.assignments;
 
 public class Assignment1 {
-	public static void main(String args[])
-	{
+
+	public static void main(String[] args) {
 		
+		
+		
+		//Temperature of a city in degrees Celsius: 25.5
+		float temperature = 25.5f;
+		//Whether a customer has placed an order: true or false
+		boolean isOrderPlaced = false;
+		//Person's phone number: "123-456-7890"
+		long phoneNumber = 1234567890L;
+		//Amount of money in a customer's bank account: 1000.50
+		float amount=1000.50f;
+		//Person's email address: "john.doe@example.com"
+		String emailAddress="john.doe@example.com";
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		double longitude = 37.7749;
+		double latitude= -122.4194;
+		//Person's marital status: true or false
+		boolean maritalStatus = true;
+		//Person's occupation: "Software Engineer"
+		String occupation = "Software Engineer";
+		//Person's favourite colour: "Blue"
+		String favColor = "Blue";
+		//Current year: 2023
+		short currentYear = 2023;
+		//Number of followers on a social media platform: 1,000,000
+		int noOfFollowers = 100000;
+		//Rating of a movie: 7.5
+		float movieRating = 7.5f;
+		//Person's blood type: 'A'
+		char bloodType = 'A';
+		//Title of a book: "To Kill a Mockingbird"
+		String bookTitle = "To Kill a Mockingbird";
+		//Number of employees in a company: 500
+		short noOfEmployees= 500;
+		//Time of an event: 2:30 PM
+		String eventTime = "2:30 PM";
+		//Name of a country: "United States"
+		String nameOfCountry = "UnitedStates";
+	    //Person's eye color: "Brown"
+		String eyeColor = "Brown";
+		//Person's birthplace: "New York City"
+		String birthPlace = "New York City";
+		//Distance between two cities: 200.5
+		float distanceBetweenCities = 200.5f;
+		
+		
+
 	}
 
 }
